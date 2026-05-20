5c3ef322bb029c217535cd2296b72be7b17a43e2
lightget2099 <aru54krat@gmail.com>
2026-05-20 22:17:47 +0300

    test app commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,10 @@
 package org.example;
 
 
+import org.example.entity.Client;
+import org.example.entity.Planet;
+import org.example.service.ClientCrudService;
+import org.example.service.PlanetCrudService;
 import org.flywaydb.core.Flyway;
 
 public class Main {
@@ -16,5 +20,59 @@ public class Main {
         initMigration();
         System.out.println("Flyway ініціював і заповнив БД");
 
+        ClientCrudService clientService = new ClientCrudService();
+        PlanetCrudService planetService = new PlanetCrudService();
+
+        System.out.println("======== Тестування Client ========");
+
+        // create
+        Client newClient = new Client();
+        newClient.setName("Богдан");
+        clientService.save(newClient);
+        System.out.println("Створили клієнта з id: " + newClient.getId());
+
+        // read
+        Client foundClient = clientService.findById(newClient.getId());
+        if (foundClient != null) {
+            System.out.println("Знайдено клієнта: " + foundClient.getName());
+        }
+
+        // update
+        if (foundClient != null) {
+            foundClient.setName("Богдан онолвненний");
+            clientService.update(foundClient);
+            System.out.println("Ім'я після оновлення: " + clientService.findById(foundClient.getId()).getName());
+        }
+
+        // delete
+        if (foundClient != null) {
+            clientService.deleteById(foundClient.getId());
+            System.out.println("Клієнта видалено. Пошук дає: " + clientService.findById(foundClient.getId()));
+        }
+
+        System.out.println("============== Тест Planet =================");
+
+        Planet newPlanet = new Planet();
+        newPlanet.setId("PLN1");
+        newPlanet.setName("Нова Земля");
+        planetService.save(newPlanet);
+        System.out.println("Створили планету: " + newPlanet.getId() + " - " + newPlanet.getName());
+
+        // read
+        Planet foundPlanet = planetService.findById("PLN1");
+        if (foundPlanet != null) {
+            System.out.println("Знайдено планету: " + foundPlanet.getName());
+        }
+
+        // update
+        if (foundPlanet != null) {
+            foundPlanet.setName("Нова Земля v2.0");
+            planetService.update(foundPlanet);
+            System.out.println("Назва після оновлення: " + planetService.findById("PLN1").getName());
+        }
+
+        // delete
+        planetService.deleteById("PLN1");
+        System.out.println("Планету видалено. Шукаємо видаленну планету: " + planetService.findById("PLN1"));
     }
 }
